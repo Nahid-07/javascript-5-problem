ccc2ae1885d896885705111de79d9e2511813360
Nahid-07 <nahidasif1998@gmail.com>
2022-08-07 19:23:55 +0600

    problem solved

---
+++ finding-smallest-number.js
@@ -0,0 +1,7 @@
+/* একটা কোড লিখো। যেটা দিয়ে কোন একটা array এর মধ্যে সবচেয়ে ছোট সংখ্যা বের করে দিতে পারবে। */
+
+let array  = [25,22,23,56,85,65,58];
+let min = Math.min(...array);
+console.log(min);
+let max = Math.max(...array);
+console.log(max);
